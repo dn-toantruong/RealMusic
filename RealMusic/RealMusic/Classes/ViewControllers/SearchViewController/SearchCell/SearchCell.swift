256e12784aecbe9af29d28d57da392056c3b06e2
asiantech <truongtoandn@gmail.com>
2016-08-30 16:35:26 +0700

    show data in SeachVC

--- RealMusic/RealMusic/Classes/ViewControllers/SearchViewController/SearchCell/SearchCell.swift
+++ RealMusic/RealMusic/Classes/ViewControllers/SearchViewController/SearchCell/SearchCell.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class SearchCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageSong: UIImageView!
+    @IBOutlet weak var artistLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +24,21 @@ class SearchCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configCell(object: Song) {
+        self.nameLabel.text = object.title
+        self.artistLabel.text = object.user.username
+        let thumb = NSURL(string: object.artwork_url)!
+        let request = NSURLRequest(URL: thumb)
+        self.imageSong.af_setImageWithURLRequest(request, placeholderImage: nil, filter: nil, progress: nil, progressQueue: dispatch_get_main_queue(), imageTransition: UIImageView.ImageTransition.CrossDissolve(0.5), runImageTransitionIfCached: true) { (response) in
+            switch response.result {
+            case .Success(let value):
+                print("success: \(value)")
+            case .Failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 }
